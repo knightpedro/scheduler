c492daf788da9518bf5be3122dbb211de1531f07
Peter Knight <petekanighit@gmail.com>
2019-10-28 12:22:38 +1300

    Added Web API project.

---
+++ Scheduler.Persistence/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scheduler.Application.Interfaces;
+
+namespace Scheduler.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<SchedulerDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("SchedulerDatabase")));
+
+            services.AddScoped<ISchedulerDbContext>(provider => provider.GetService<SchedulerDbContext>());
+            return services;
+        }
+    }
+}
