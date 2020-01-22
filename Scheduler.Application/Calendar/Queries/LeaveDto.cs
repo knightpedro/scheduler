cd690ecefb0c49488cd91506fa4b6ec1db65dacf
Peter Knight <petekanighit@gmail.com>
2020-01-22 20:22:42 +1300

    Added leave type to DTO.

--- Scheduler.Application/Calendar/Queries/LeaveDto.cs
+++ Scheduler.Application/Calendar/Queries/LeaveDto.cs
@@ -6,12 +6,14 @@ namespace Scheduler.Application.Calendar.Queries
     public class LeaveDto
     {
         public int Id { get; set; }
+        public string LeaveType { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
 
         public LeaveDto(Leave leave)
         {
             Id = leave.Id;
+            LeaveType = leave.LeaveCategory.ToString();
             Start = leave.LeavePeriod.Start;
             End = leave.LeavePeriod.End;
         }
