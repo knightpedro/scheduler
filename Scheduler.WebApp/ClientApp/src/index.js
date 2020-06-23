851f0c447bb0325ff4987f809656342f09b9f757
Peter Knight <petekanighit@gmail.com>
2020-06-23 20:38:36 +1200

    Added immer work around for Azure pipeline build

--- Scheduler.WebApp/ClientApp/src/index.js
+++ Scheduler.WebApp/ClientApp/src/index.js
@@ -4,6 +4,7 @@ import App from "./app";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import reducer from "./ducks";
+import { enableES5 } from "immer";
 
 import "react-datepicker/dist/react-datepicker.css";
 import "react-dates/initialize";
@@ -11,6 +12,7 @@ import "react-dates/lib/css/_datepicker.css";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 
+enableES5();
 const store = configureStore({ reducer });
 
 render(
